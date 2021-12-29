b512f273f5d3c369406ed51f34a19cd51b48daae
hacker <594524502@qq.com>
2021-12-29 12:44:54 +0800

    update

--- Encryption/main.cpp
+++ Encryption/main.cpp
@@ -6,8 +6,6 @@ std::wstring decode(const std::wstring& str);
 
 int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
-	auto&& aa = encode(L"Œ‘≤€a");
-	auto&& aaa = decode(aa);
 	App::Initialize();
 	float y = 460;
 	Button* b = new Button();
